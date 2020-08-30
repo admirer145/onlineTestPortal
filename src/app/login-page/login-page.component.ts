ace331527d3447954c855ebe01b91e0cb96a5a49
narendra <narendradmirer145@gmail.com>
2020-08-30 22:43:16 +0530

    make register form reactive

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -9,16 +9,16 @@ import {CustomFormValidations} from '../custom-form-validations'
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-  loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required, CustomFormValidations.emailValidation]),
-      password: new FormControl('', [Validators.required, CustomFormValidations.passwordValidation, Validators.minLength(8)])
-  });
-
+  loginForm;
   constructor(public router: Router, public manageQuestionsServive: ManageQuestionsService) {
     manageQuestionsServive.resetUserData();
   }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, CustomFormValidations.emailValidation]),
+      password: new FormControl('', [Validators.required, CustomFormValidations.passwordValidation, Validators.minLength(8)])
+    });
   }
 
   loginToStartPage(): void{
--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {CustomFormValidations} from '../custom-form-validations';
 
 @Component({
   selector: 'app-register-page',
@@ -9,15 +11,31 @@ import {Router} from '@angular/router';
 export class RegisterPageComponent implements OnInit {
   userDetails;
   constructor(public router: Router) {
-    this.userDetails = {
-      name : '',
-      emailid : '',
-      password : '',
-      age : ''
-    };
+    // this.userDetails = {
+    //   name : '',
+    //   emailid : '',
+    //   password : '',
+    //   age : ''
+    // };
   }
 
   ngOnInit(): void {
+    this.userDetails = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, CustomFormValidations.emailValidation]),
+      password: new FormControl('', [Validators.required, CustomFormValidations.passwordValidation, Validators.minLength(6)]),
+      age: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      college: new FormControl('', Validators.required),
+      cgpa: new FormControl('', Validators.required),
+      address: new FormGroup({
+        street: new FormControl(''),
+        city: new FormControl('', Validators.required),
+        state: new FormControl('', Validators.required),
+        zipcode: new FormControl('', Validators.required)
+      })
+    });
   }
   submitEventHandler(): void{
     alert('Registration Successful');
